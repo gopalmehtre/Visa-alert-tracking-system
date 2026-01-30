63f36615a8c04bedd6402c6d928ef589a12ded4b
Gopal <gopalmehtre10@gmail.com>
2026-01-31 01:34:27 +0530

    cors-issues

--- frontend/src/api/alertApi.js
+++ frontend/src/api/alertApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/alerts";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/alerts";
 
 export const getAlerts = async () => {
   const res = await axios.get(API_URL);
